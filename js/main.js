75601f980b9a09843b4ab96edf2aebb3ec91ff8f
Brian P. Holt <bholt@planetholt.com>
2013-12-14 20:12:01 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,95 @@
+
+(function (buttonID, $) {
+    'use strict';
+
+    $.extend({
+        interpolate: function (s, data) {
+            var variable;
+            for (variable in data) {
+                if (data.hasOwnProperty(variable)) {
+                    s = s.replace(new RegExp('{' + variable + '}', 'g'), data[variable]);
+                }
+            }
+
+            return s;
+        }
+    });
+
+    function handleGallery2Response(data) {
+        var lines = data.split('\n'),
+            pattern = /^image\.(.+)\.(\d+)$/,
+            template = '<a href="/gallery2/d/{name}-2/{title}" title="{description}" rel="lightbox[g2image]"><img src="/gallery2/d/{resized.2.name}-2/{title}" srcset="/gallery2/d/{resized.2.name}-2/{title} 1x, /gallery2/d/{resized.1.name}-2/{title} 2x" alt="{caption}" width="{resized.2.width}" height="{resized.2.height}"/></a>',
+            images = [];
+
+        $('#container').empty();
+
+        $.each(lines, function () {
+            var keyVal = this.split('='),
+                parsedKey,
+                fieldName,
+                imageIndex,
+                image;
+
+            if (pattern.test(keyVal[0])) {
+                parsedKey = pattern.exec(keyVal[0]);
+                fieldName = parsedKey[1];
+                imageIndex = parsedKey[2] - 1;
+
+                image = images[imageIndex] || {};
+
+                image[fieldName] = keyVal[1];
+
+                images[imageIndex] = image;
+            }
+        });
+
+        $.each(images, function (i) {
+            if (this['extrafield.Description']) {
+                this.description = this['extrafield.Description'];
+            } else {
+                this.description = this.caption;
+            }
+
+            if (!this['resized.1.name']) {
+                console.warn($.interpolate('{title} with ID {name} is missing HiDPI resize', this));
+            }
+
+            if (!this['resized.2.name']) {
+                console.warn($.interpolate('{title} with ID {name} is missing default resize', this));
+            }
+
+            images[i] = this;
+        });
+
+        $.each(images, function () {
+            var wrapperTmpl = '<li><span class="image">{img}</span><span class="description"><h2>{caption}</h2><div>{description}</div><span class="html">{html}</span></span></li>',
+                inter = $.extend({
+                    img: $.interpolate(template, this),
+                    html: ''
+                }, this),
+                $dom = $($.interpolate(wrapperTmpl, inter));
+
+            $dom.find('span.html').text(inter.img);
+
+            $('#container').append($dom);
+        });
+    }
+
+    $(document).ready(function() {
+	    $('form').on('submit', function (e) {
+		    $.post('/gallery2/main.php?g2_controller=remote:GalleryRemote', {
+		        'g2_form[cmd]': 'fetch-album-images',
+		        'g2_form[protocol_version]': '2.4',
+		        'g2_form[set_albumName]': $('#galleryID').val(),
+		        'g2_form[albums_too]': 'no',
+		        'g2_form[random]': 'no',
+		        'g2_form[extrafields]': 'yes',
+		        'g2_form[all_sizes]': 'yes'
+		    }, handleGallery2Response);
+
+                e.preventDefault();
+	    });
+            $('#galleryID').focus();
+    });
+}('#doIt', jQuery));
+
